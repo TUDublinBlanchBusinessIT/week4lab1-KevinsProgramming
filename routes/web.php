0b7dd18536be0ee3b465b735c3b308e465917ad5
B00157434 Kevin Tshiaka <B00157434@mytudublin.ie>
2025-02-17 16:10:26 +0000

    new

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
+
 Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
 Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create');
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
